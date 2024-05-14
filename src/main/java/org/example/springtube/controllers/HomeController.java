b86d77d3d9de07a5b2fcb5f7aaf015d16335f86f
kauroah <kauroah@gmail.com>
2024-05-14 16:02:22 +0300

    SEARCH AND FIXING 2T

--- src/main/java/org/example/springtube/controllers/HomeController.java
+++ src/main/java/org/example/springtube/controllers/HomeController.java
@@ -10,8 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 
@@ -43,6 +46,12 @@ public class HomeController {
     @GetMapping("/springtube")
     public String home(Model model, @RequestParam(required = false) String query) {
         List<Video> videos = videoService.searchVideos(query);
+        model.addAttribute("videos", videoService.findAll());
+
+        if (videos.isEmpty()) {
+            String youtubeSearchUrl = "https://www.youtube.com/results?search_query=" + URLEncoder.encode(query, StandardCharsets.UTF_8);
+            return "redirect:" + youtubeSearchUrl;
+        }
         model.addAttribute("videos", videos);
 
         return "home";
